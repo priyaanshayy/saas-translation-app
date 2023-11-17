74399acf0ed3a4277b68d07423b25eb26b9cd91a
priyaanshayy <122358408+priyaanshayy@users.noreply.github.com>
2023-11-17 21:56:53 +0530

    Update firebase-admin.ts

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -3,6 +3,12 @@ import admin from "firebase-admin";
 
 let app;
 
+const serviceAccount = {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+  };
+
 if(!admin.apps.length) {
     app=admin.initializeApp({
         credential: admin.credential.cert({
